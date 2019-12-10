f965931de17eae8ef65afdea673bd23ccaf9cf75
Horizony392 <58688440+Horizony392@users.noreply.github.com>
2019-12-10 21:35:45 +0800

    Add files via upload

--- FLY.cpp
+++ FLY.cpp
@@ -1,25 +1,19 @@
 #include "FLY.h"
 #include "BLOCK.h"
-#include "basic_define.h"
 #include <graphics.h>
 
 extern BLOCK* FindBlock(int index);
+extern std::list<class FLY>	list_fly_null;
 
-static struct FLY_TYPE
-{
-	int		size_x;				//子弹的x大小
-	int		size_y;				//子弹的y大小
-	int		speed;				//子弹的速度
-};
+FLY_TYPE fly_type[MAX_FLY];
 
-static FLY_TYPE fly_type[MAX_FLY] =
+FLY::FLY(
+	int Iid,
+	int Ix,
+	int Iy,
+	int Ifacing
+)
 {
-	{1,1,2}
-};
-
-int FLY::Setup(int Iid, int Ix, int Iy, int Ifacing)
-{
-	if (Iid >= MAX_FLY) return -1;
 	id = Iid;
 	x = Ix;
 	y = Iy;
@@ -27,30 +21,30 @@ int FLY::Setup(int Iid, int Ix, int Iy, int Ifacing)
 	size_x = fly_type[id].size_x;
 	size_y = fly_type[id].size_y;
 	speed = fly_type[id].speed;
-	return 0;
+	explosion_radium = fly_type[id].explosion_radium;
+}
+FLY::FLY()
+{
+	id = -1;
+	x = y = facing = size_x = size_y = speed = 0;
 }
 
 void FLY::Turning(int position) { facing = position; }
 
 void FLY::Move()
 {
-	int t_x = x, t_y = y;
-	int move_x = 0, move_y = 0;
-	switch (facing)
-	{
-	case UP:	move_x = -1; break;
-	case DOWN:	move_x = +1; break;
-	case LEFT:	move_y = -1; break;
-	case RIGHT:	move_y = +1; break;
-	}
+	std::list<class FLY>::iterator fly_iterator;
+	fly_iterator = map[ChangeToScreen(x)][ChangeToScreen(y)].fly;
+	ClearIterator();
 	for (int i = 0; i < speed; i++)
 	{
-		t_x += move_x;
-		t_y += move_y;
+		int t_x = x + dir[facing].x;
+		int t_y = y + dir[facing].y;
 		if (!CanStand(t_x, t_y)) break;
 		x = t_x;
 		y = t_y;
 	}
+	SetIterator(fly_iterator);
 }
 
 bool FLY::CanStand(int x, int y)
@@ -76,8 +70,53 @@ void FLY::FlyClear() {
 	}
 }
 
+void FLY::Paint()
+{
+#define PI 3.14159265
+	IMAGE print_picture = NULL;
+	switch (facing)
+	{
+	case UP:
+		print_picture = picture;
+		break;
+	case DOWN:
+		rotateimage(&print_picture, &picture, PI);
+		break;
+	case LEFT:
+		rotateimage(&print_picture, &picture, PI / 2);
+		break;
+	case RIGHT:
+		rotateimage(&print_picture, &picture, -PI / 2);
+		break;
+	}
+	putimage(ScreenXPixel(x), ScreenYPixel(y), &print_picture);
+#undef PI
+}
+
 int FLY::Getx() { return x; }
 int FLY::Gety() { return y; }
 int FLY::GetxEnd() { return x + size_x; }
 int FLY::GetyEnd() { return y + size_y; }
 int FLY::Getid() { return id; }
+
+void FLY::SetIterator(std::list<class FLY>::iterator ite_fly)
+{
+	int min_x = ChangeToScreen(x);
+	int min_y = ChangeToScreen(y);
+	int max_x = ChangeToScreen(x + size_x * BLOCK_SIZE - 1);
+	int max_y = ChangeToScreen(y + size_y * BLOCK_SIZE - 1);
+	for (int i = min_x; i <= max_x; i++)
+		for (int j = min_y; j <= max_y; j++)
+			map[i][j].fly = ite_fly;
+}
+
+void FLY::ClearIterator()
+{
+	int min_x = ChangeToScreen(x);
+	int min_y = ChangeToScreen(y);
+	int max_x = ChangeToScreen(x + size_x * BLOCK_SIZE - 1);
+	int max_y = ChangeToScreen(y + size_y * BLOCK_SIZE - 1);
+	for (int i = min_x; i <= max_x; i++)
+		for (int j = min_y; j <= max_y; j++)
+			map[i][j].fly = list_fly_null.end();
+}
--- FLY.h
+++ FLY.h
@@ -2,11 +2,13 @@
 #ifndef FLY_INCLUDED
 #define FLY_INCLUDED
 
-#define MAX_FLY 1
+#include "basic_define.h"
+#define MAX_FLY 3
 
 class FLY
 {
 private:
+	IMAGE	picture;			//子弹的贴图
 	int		id;					//子弹的ID
 	int		x;					//子弹的x坐标(像素坐标)
 	int		y;					//子弹的y坐标(像素坐标)
@@ -14,17 +16,56 @@ private:
 	int		size_y;				//子弹的y大小
 	int		facing;				//子弹的朝向
 	int		speed;				//子弹的速度
+	int		explosion_radium;	//子弹的爆破半径
+	void	ClearIterator();
 public:
+	FLY(
+		int Iid,
+		int Ix,
+		int Iy,
+		int Ifacing);
+	FLY();
 	bool	CanStand(int x, int y);
-	int		Setup(int Iid, int Ix, int Iy, int Ifacing);
 	void	Turning(int position);
 	void	Move();
+	void	Paint();
 	void    FlyClear();
 	int		Getx();
 	int		Gety();
 	int		GetxEnd();
 	int		GetyEnd();
 	int		Getid();
+	void	SetIterator(std::list<class FLY>::iterator ite_fly);
+};
+
+struct FLY_TYPE
+{
+	IMAGE	picture;			//子弹贴图
+	int		size_x;				//子弹的x大小
+	int		size_y;				//子弹的y大小
+	int		speed;				//子弹的速度
+	int		explosion_radium;	//子弹的爆破半径			
+	FLY_TYPE(
+		LPCTSTR	Lpicture,
+		int		Isize_x,
+		int		Isize_y,
+		int		Ispeed,
+		int		Iexplosion_radium
+	)
+	{
+		loadimage(&picture, Lpicture);
+		size_x = Isize_x;
+		size_y = Isize_y;
+		speed = Ispeed;
+		explosion_radium = Iexplosion_radium;
+	}
+	FLY_TYPE()
+	{
+		picture = NULL;
+		size_x = 0;
+		size_y = 0;
+		speed = 0;
+	}
 };
 
 #endif
